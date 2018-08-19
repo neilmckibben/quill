704038df7a684f20c3a1f18f3d574e8471da7cc7
alvin-law <law.185@osu.edu>
2018-08-19 17:58:27 -0400

    Fixed application necessary fields check

--- app/server/models/User.js
+++ app/server/models/User.js
@@ -355,7 +355,7 @@ schema.statics.validateProfile = function(profile, cb){
     profile.adult &&
     profile.school.length > 0 &&
 	profile.major.length > 0 &&
-    ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'].indexOf(profile.graduationMonth) > -1
+    ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'].indexOf(profile.graduationMonth) > -1 &&
     ['2018', '2019', '2020', '2021', '2022'].indexOf(profile.graduationYear) > -1 &&
     ['M', 'F', 'O', 'N'].indexOf(profile.gender) > -1
     ));
